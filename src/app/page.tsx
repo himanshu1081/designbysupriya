94ce6f2c83aa50c8a07839fa44e309b62c7850b2
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2026-01-13 21:50:25 +0530

    Made contact section responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <Portfolio />
       <Services />
       <Contact />
-      {/* <ContactInfo/> */}
     </div>
   );
 }
--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -7,43 +7,45 @@ import { FaPinterest } from "react-icons/fa";
 
 const ContactInfo = () => {
 
+    const size:number = window.innerWidth < 450 ? 10 : 20;
+
     return (
-        <div className="w-full flex justify-between items-start px-20 pb-5 text-black/50 bg-[#f9f8f6]">
-            <div className="flex flex-col w-30 justify-center items-start ">
-                <span className="font-georgia font-bold text-2xl text-black">
+        <div className="w-full flex flex-col lg:flex-row justify-between items-start p-3 text-black/50 ">
+            <div className="flex flex-col w-fit justify-center items-start ">
+                <span className="font-georgia font-bold text-base lg:text-2xl text-black">
                     Studio
                 </span>
-                <span className="text-sm">
+                <span className="text-xs lg:text-base">
                     147 West 24th Street
                     New York, NY 10011
                 </span>
             </div>
-            <div className="flex flex-col w-30 justify-center items-start ">
-                <span className="font-georgia font-bold text-2xl text-black">
+            <div className="flex flex-col w-fit justify-center items-start ">
+                <span className="font-georgia font-bold text-black text-base lg:text-2xl">
                     Hours
                 </span>
-                <div className="flex flex-col text-sm">
+                <div className="flex flex-col text-xs lg:text-sm">
                     <span className="whitespace-nowrap">
                         Monday – Friday
                     </span>
-                    <span className="whitespace-nowrap">
+                    <span className="whitespace-nowrap text-xs lg:text-base">
                         9:00 AM – 6:00 PM
                     </span>
                 </div>
             </div>
-            <div className="flex flex-col w-30 justify-center items-start">
-                <span className="font-georgia font-bold text-2xl text-black">
+            <div className="flex flex-col w-fit justify-center items-start">
+                <span className="font-georgia font-bold text-base lg:text-2xl text-black">
                     Connect
                 </span>
-                <div className="text-sm flex flex-col gap-2 ">
+                <div className="text-xs lg:text-sm flex flex-col gap-2 ">
                     <span className="hover:text-black cursor-pointer flex gap-1 items-center justify-start">
-                        <AiOutlineInstagram size={20} />
+                        <AiOutlineInstagram size={size} />
                         <span>
                             INSTAGRAM
                         </span>
                     </span>
                     <span className="hover:text-black cursor-pointer flex gap-1 items-center justify-start">
-                        <FaPinterest size={20} />
+                        <FaPinterest size={size} />
                         <span>
                             PINTEREST
                         </span>
